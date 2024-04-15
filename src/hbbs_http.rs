ff9b4b5af6374037d131e7ce2db55646a885ef1d
fufesou <shuanglongchen@yeah.net>
2024-04-15 22:55:46 +0800

    Fix. Custom oidc, https, logs
    Signed-off-by: fufesou <shuanglongchen@yeah.net>

--- src/hbbs_http.rs
+++ src/hbbs_http.rs
@@ -19,7 +19,20 @@ impl<T: DeserializeOwned> TryFrom<Response> for HbbHttpResponse<T> {
     type Error = reqwest::Error;
 
     fn try_from(resp: Response) -> Result<Self, <Self as TryFrom<Response>>::Error> {
-        let map = resp.json::<Map<String, Value>>()?;
+        let status = resp.status();
+        let text = resp.text()?;
+        let map: Map<String, Value> = match serde_json::from_str(&text) {
+            Ok(v) => v,
+            Err(e) => {
+                hbb_common::log::error!(
+                    "Failed to parse response, code: {}, text: {:?}, decode error: {:?}",
+                    status,
+                    &text,
+                    e
+                );
+                return Ok(Self::Error(e.to_string()));
+            }
+        };
         if let Some(error) = map.get("error") {
             if let Some(err) = error.as_str() {
                 Ok(Self::Error(err.to_owned()))
